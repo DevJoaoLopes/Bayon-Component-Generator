5ed95ce6f198099707019ff9de83633d26289423
joao lopes <joao.lopes@softplan.com.br>
2024-01-07 23:51:44 -0300

    wip: create folder and components react

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	const createBayonReactComponent = async (path: string, nameComponent: string) => {
 
-	const createComponentDir = (name?: string) => {
-		// vscode.workspace.fs
-		// console.log(vscode.window.activeTextEditor?.document.uri.path);
-		// console.log(vscode.workspace.workspaceFolders);
-		// console.log(vscode.workspace.workspaceFolders?.[0].uri.fsPath);
+
+		if(!vscode.workspace.fs.isWritableFileSystem('file')) {
+			return vscode.window.showErrorMessage('Sistema nao permite a criacao de arquivos !!');
+		}
+
+		const uriParse = vscode.Uri.parse(path + '/' + nameComponent + '/' + nameComponent + '.tsx'); //!mudar extensao e criar mais de um arquivo dentro de uma pasta
+		vscode.workspace.fs.writeFile(uriParse, new TextEncoder().encode('conteudo do component'));
 	};
 
-
-	// const createReactComponent = (uri: string, nameComponent: string) => {
-
-	// }
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -29,7 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Generate from bayon-component-generator!');
-		createComponentDir('teste');
 
 		let inputBoxField = await vscode.window.showInputBox({
 			placeHolder: "Digite o nome do componente",
@@ -40,8 +35,8 @@ export function activate(context: vscode.ExtensionContext) {
 		if(!inputBoxField || inputBoxField?.trim().length === 0) {
 			vscode.window.showErrorMessage('Nome digitado nao eh valido!!');
 		} else {
-			//! Aqui vai chamar a funcao que criara o componente de fato
-			vscode.workspace.fs.writeFile(vscode.Uri.parse(resource.path + '/' +   inputBoxField + '.txt'), new TextEncoder().encode('Hello World'));
+			// vscode.workspace.fs.writeFile(vscode.Uri.parse(resource.path + '/' +   inputBoxField + '.txt'), new TextEncoder().encode('Hello World'));
+			createBayonReactComponent(resource?.path, inputBoxField);
 			vscode.window.showInformationMessage(`Criado componente ${inputBoxField}`);
 		}
 	});
