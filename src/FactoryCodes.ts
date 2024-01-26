bc72eb2d4e74fd8cb68eb310d9b62d6337d20583
joao lopes <joao.lopes@softplan.com.br>
2024-01-25 23:27:08 -0300

    feat: add template for generate code

---
+++ src/FactoryCodes.ts
@@ -0,0 +1,71 @@
+type ITypes = 'COMPONENT' | 'STYLES' | 'INDEX' | 'TEST';
+
+
+const codeTsComponent = (nameComponent: string) => (
+`import React from 'react';
+import * as Styles from './${nameComponent}.styles'
+
+export const ${nameComponent} = () => {
+    return (
+        <Styles.Container>
+          <div></div>
+        </Styles.Container>
+    );
+};
+`
+);
+
+
+const codeStyleComponent = () => (
+`import { styled } from '@bayon/commons';
+
+export const Container = styled('div')(() => ({
+  display: 'flex',
+  boxSizing: 'border-box',
+}));
+`
+);
+
+
+const codeIndex = (nameComponent: string) => (
+`export { ${nameComponent} } from './${nameComponent}';
+`
+);
+
+
+const codeTestComponent = (nameComponent: string) => (
+`import React from 'react';
+import { render, screen } from '@bayon/testing';
+
+import { ${nameComponent} } from './${nameComponent}';
+
+describe('<${nameComponent} />', () => {
+  it('should render component', () => {
+    createComponent();
+    expect(screen.getByText('')).toBeInTheDocument();
+  });
+});
+
+const createComponent = (props = {}) => {
+  const defaultProps = {
+    ...props,
+  };
+
+  return render(<${nameComponent} {...defaultProps} />);
+};
+`
+);
+
+
+
+
+export const getTemplates = (name: string, type: ITypes) => {
+    const dictionaryTemplate = {
+        'COMPONENT': codeTsComponent(name),
+        'STYLES': codeStyleComponent(),
+        'INDEX': codeIndex(name),
+        'TEST': codeTestComponent(name)
+    };
+
+    return dictionaryTemplate[type];
+};
